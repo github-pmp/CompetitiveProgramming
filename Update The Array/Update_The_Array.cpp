23a4f07f9652b60ba01241a2428780f60516d467
Pasquale <62596251+github-pmp@users.noreply.github.com>
2020-11-02 17:33:20 +0100

    Create Update_The_Array.cpp

---
+++ Update The Array/Update_The_Array.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+
+void add(std::vector<long long> &b,long long index,long long val){
+	index = index + 1; 
+    while (index <= b.capacity()) { 
+    	b[index] += val; 
+    	index += index & (-index); 
+	}
+}
+
+long long sum(std::vector<long long> &b,long long index){
+	long long sum = 0; 
+    index = index + 1; 
+    while (index>0) 
+    { 
+        sum += b[index]; 
+        index -= index & (-index); 
+    } 
+    return sum;
+}
+
+int main() {
+	int test,dim,l,r,numq;
+	long long nupdate,num;
+	std::vector<long long> result;	
+	std::cin>> test;
+	while(test > 0){
+	std::cin >> dim;
+	std::cin >> nupdate;
+	std::vector<long long> v;
+	v.reserve(dim);
+	result.reserve(dim);
+	std::vector<long long> BITtree(dim+1);
+	for(long long i=0; i<nupdate ;i++){
+		std::cin >> l;
+		std::cin >> r;
+		std::cin >> num;
+		add(BITtree,l,num);
+		if(r!=dim-1)add(BITtree,r+1,-num);
+	}
+	std::cin>>numq;
+	int query;
+	for(int i=0;i<numq;i++){
+		std::cin >> query;
+		result.push_back(sum(BITtree,query));
+	}
+	test--;	
+	}
+	for(int i=0; i<result.size() ;i++)	
+		std::cout << result[i] << std::endl;
+	
+	return 0;
+}
