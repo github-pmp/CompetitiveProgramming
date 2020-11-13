16e35ab47d6f4021f723acc19e3704636bcab18c
Pasquale <62596251+github-pmp@users.noreply.github.com>
2020-11-13 19:33:54 +0100

    Update Towers.cpp

--- Towers/Towers.cpp
+++ Towers/Towers.cpp
@@ -1,8 +1,8 @@
 /*TIME COMPLEXITY*/
 /*For resolve this problem i use this approach:
--In the first step i order the array and this cost O(logN), in this way equal elements are near.
+-In the first step i order the array and this cost O(N*logN), in this way equal elements are near.
 -In the second step we count the equal elements that are near and compute the maximum of them, this cost ϴ(N) in time.
-The total cost is O(logN)+ϴ(N) time.*/
+The total cost is O(N*logN)+ϴ(N) time.*/
 
 
 #include<stdio.h>
