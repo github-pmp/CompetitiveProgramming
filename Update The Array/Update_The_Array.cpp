e5989d4c56f748fbe79d0ef27b5933a70c79052e
Pasquale <62596251+github-pmp@users.noreply.github.com>
2020-11-16 18:41:18 +0100

    Update Update_The_Array.cpp

--- Update The Array/Update_The_Array.cpp
+++ Update The Array/Update_The_Array.cpp
@@ -1,3 +1,10 @@
+/*TIME COMPLEXITY*/
+/*To resolve this problem i use an auxiliary data structure called "binary indexed tree"(BIT) to store the dynamic prefix-sums.
+The idea is to support the update operation and have a better time for the query.
+For one update operation we have to sum all the element in the range and we pay at most O(N) time.
+The update operation considering that we use a BIT cost at most O(LogN) where N is the index of the query.
+In conclusion this algorithm cost O(N) + O(LogN) time.
+*/
 #include <stdio.h>
 #include <iostream>
 #include <vector>
@@ -5,7 +12,7 @@
 
 
 void add(std::vector<long long> &b,long long index,long long val){
-	index = index + 1; 
+    index = index + 1; 
     while (index <= b.capacity()) { 
     	b[index] += val; 
     	index += index & (-index); 
@@ -13,7 +20,7 @@ void add(std::vector<long long> &b,long long index,long long val){
 }
 
 long long sum(std::vector<long long> &b,long long index){
-	long long sum = 0; 
+    long long sum = 0; 
     index = index + 1; 
     while (index>0) 
     { 
